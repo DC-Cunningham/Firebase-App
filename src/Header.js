c20e7d3c7f5369cf2a9e5c88d3034175265ae78b
DCRevResLabs <dc@revolutionresearch.com.au>
2020-10-20 10:18:57 +1100

    created logout

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import {useHistory} from 'react-router-dom'
+
+import { logout} from './firebase/auth'
+import { useSession } from './firebase/UserProvider'
 
 function Header() {
+const history = useHistory();
+const {user} = useSession();
+
+const logoutUser = async () => {
+await logout();
+history.push('/signup');
+
+};
   return (
     <header>
       <h2>The Grid</h2>
+      {!!user && <button className="ui secondary button logout" onClick={logoutUser}>LOGOUT</button>}
     </header>
   )
 }
