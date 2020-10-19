d29b3f47482e75f506eb36999f966a6dd65632c8
DCRevResLabs <dc@revolutionresearch.com.au>
2020-10-19 23:12:18 +1100

    auth integration

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 
-function Signup() {
+import { signup } from "../firebase/auth";
+
+function Signup(props) {
+  const { register, handleSubmit, reset } = useForm();
+  const [isLoading, setLoading] = useState(false);
+
+  const onSubmit = async (data) => {
+    let newUser;
+    setLoading(true);
+    try {
+      newUser = await signup(data);
+      reset();
+    } catch (error) {
+      console.log(error);
+    }
+    if (newUser) {
+      props.history.push(`/profile/${newUser.uid}`);
+    } else {
+      setLoading(false);
+    }
+  };
+
+  const formClassName = `ui form ${isLoading ? "loading" : ""}`;
+
   return (
     <div className="login-container">
       <div className="ui card login-card">
         <div className="content">
-          <form className="ui form">
+          <form className={formClassName} onSubmit={handleSubmit(onSubmit)}>
             <div className="two fields">
               <div className="field">
                 <label>
@@ -14,26 +38,42 @@ function Signup() {
                     type="text"
                     name="firstName"
                     placeholder="First Name"
+                    ref={register}
                   />
                 </label>
               </div>
               <div className="field">
                 <label>
                   Last Name
-                  <input type="text" name="lastName" placeholder="Last Name" />
+                  <input
+                    type="text"
+                    name="lastName"
+                    placeholder="Last Name"
+                    ref={register}
+                  />
                 </label>
               </div>
             </div>
             <div className="field">
               <label>
                 Email
-                <input type="email" name="email" placeholder="Email" />
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  ref={register}
+                />
               </label>
             </div>
             <div className="field">
               <label>
                 Password
-                <input type="password" name="password" placeholder="Password" />
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  ref={register}
+                />
               </label>
             </div>
             <button className="ui primary button login" type="submit">
